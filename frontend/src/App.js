25380a4ebc28535124ee6c73c6e24d67b51d47ab
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2023-04-07 23:03:35 +0000

    two

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import "./App.css";
 import Footer from "./components/footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
           <h1>Ecommers App</h1>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" Component={HomeScreen} />
+          </Routes>
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
